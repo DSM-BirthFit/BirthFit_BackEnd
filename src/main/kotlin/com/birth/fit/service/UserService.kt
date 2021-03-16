5f3d5be73105a55cfb9a8eb2f7665a5f930591b6
Hanif <marbling1293@naver.com>
2021-03-16 12:50:53 +0900

    [ADD] login 로직

--- src/main/kotlin/com/birth/fit/service/UserService.kt
+++ src/main/kotlin/com/birth/fit/service/UserService.kt
@@ -1,14 +1,51 @@
 package com.birth.fit.service
 
+import com.birth.fit.config.jwt.JwtTokenProvider
 import com.birth.fit.domain.repository.UserRepository
 import com.birth.fit.dto.JoinRequest
+import com.birth.fit.dto.LoginRequest
+import com.birth.fit.exception.error.AuthorizationException
+import com.birth.fit.exception.error.LoginFailedException
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
+import java.lang.Exception
 
 @Service
 class UserService(
-    val userRepository: UserRepository
+    @Autowired val userRepository: UserRepository,
+    @Autowired val authenticationManager: AuthenticationManager,
+    @Autowired val bCryptPasswordEncoder: BCryptPasswordEncoder,
+    @Autowired val jwtTokenProvider: JwtTokenProvider
 ) {
     fun join(joinRequest: JoinRequest) {
-        userRepository.save(joinRequest.getData())
+        joinRequest.password = bCryptPasswordEncoder.encode(joinRequest.password)
+        userRepository.save(joinRequest.getData(joinRequest))
+    }
+
+    fun login(loginRequest: LoginRequest): MutableMap<String, Any> {
+        try{
+            val username: String = loginRequest.email
+            val password: String = loginRequest.password
+            val authenticator = UsernamePasswordAuthenticationToken(username, password)
+
+            authenticationManager.authenticate(authenticator)
+
+            val accessToken: String = jwtTokenProvider.createAccessToken(username)
+            val refreshToken: String = jwtTokenProvider.createRefreshToken(username)
+
+            val model: MutableMap<String, Any> = HashMap()
+            model["accessToken"] = accessToken
+            model["refreshToken"] = refreshToken
+            model["tokenType"] = "Bearer"
+
+            return model
+        }
+        catch (e: Exception) {
+            throw LoginFailedException("invalid email/password supplied")
+        }
     }
 }
