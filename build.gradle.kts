5a8b5da89e68bb6fed972d4bc718cdf5af7e86c7
Hanif <marbling1293@naver.com>
2021-03-16 12:47:21 +0900

    [ADD] security&jwt

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,11 +29,14 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("mysql:mysql-connector-java")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    compile("org.modelmapper:modelmapper:0.7.8")
+    compile("io.jsonwebtoken:jjwt:0.9.0")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
