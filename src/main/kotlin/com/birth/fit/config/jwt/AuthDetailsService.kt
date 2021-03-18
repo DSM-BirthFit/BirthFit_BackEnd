dbf1f30f2fafaa18f3529e28eacea04dcfcc056d
Hanif <marbling1293@naver.com>
2021-03-18 16:37:17 +0900

    [Delete] @Thorws

--- src/main/kotlin/com/birth/fit/config/jwt/AuthDetailsService.kt
+++ src/main/kotlin/com/birth/fit/config/jwt/AuthDetailsService.kt
@@ -6,13 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
-import kotlin.jvm.Throws
 
 @Service
 class AuthDetailsService(
     @Autowired private val userRepository: UserRepository
 ) : UserDetailsService {
-    @Throws(UsernameNotFoundException::class)
     override fun loadUserByUsername(username: String): AuthDetails {
         val user: User? = userRepository.findByEmail(username)
         user?: throw UsernameNotFoundException("cannot find such username : $username")
