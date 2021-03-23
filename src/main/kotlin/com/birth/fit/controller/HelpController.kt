99496a560cad50296b4792e6e8cbd70f60e74667
Hanif <marbling1293@naver.com>
2021-03-23 16:53:03 +0900

    [ADD] Help 글쓰기

---
+++ src/main/kotlin/com/birth/fit/controller/HelpController.kt
@@ -0,0 +1,20 @@
+package com.birth.fit.controller
+
+import com.birth.fit.dto.PostRequest
+import com.birth.fit.service.HelpService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/help")
+class HelpController(
+    @Autowired val helpService: HelpService
+) {
+
+    @PostMapping
+    fun postHelp(@RequestHeader("Authorization") bearerToken: String?,
+                 @RequestBody @Validated postRequest: PostRequest) {
+        return helpService.postHelp(bearerToken, postRequest)
+    }
+}
---
+++ src/main/kotlin/com/birth/fit/service/HelpService.kt
@@ -0,0 +1,36 @@
+package com.birth.fit.service
+
+import com.birth.fit.domain.entity.Help
+import com.birth.fit.domain.entity.User
+import com.birth.fit.domain.repository.HelpRepository
+import com.birth.fit.domain.repository.UserRepository
+import com.birth.fit.dto.PostRequest
+import com.birth.fit.exception.error.ExpiredTokenException
+import com.birth.fit.exception.error.UserNotFoundException
+import com.birth.fit.util.JwtTokenProvider
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class HelpService(
+    @Autowired val userRepository: UserRepository,
+    @Autowired val helpRepository: HelpRepository,
+    @Autowired val jwtTokenProvider: JwtTokenProvider
+) {
+
+    fun postHelp(bearerToken: String?, postRequest: PostRequest){
+        val token: String? = jwtTokenProvider.resolveToken(bearerToken)
+        if(!jwtTokenProvider.validateToken(token!!)) throw ExpiredTokenException("The token has expired.")
+
+        val user: User? = userRepository.findByEmail(jwtTokenProvider.getUsername(token))
+        user?: throw UserNotFoundException("User not found.")
+
+        helpRepository.save(
+            Help(
+                userEmail = user.email,
+                title = postRequest.title,
+                content = postRequest.content
+            )
+        )
+    }
+}
