4e494443669ef03692ea20e45bd5e4451fb5ee30
Hanif <marbling1293@naver.com>
2021-03-24 09:08:10 +0900

    [ADD] Pagination 처리 추가

--- src/main/kotlin/com/birth/fit/controller/HelpController.kt
+++ src/main/kotlin/com/birth/fit/controller/HelpController.kt
@@ -1,16 +1,28 @@
 package com.birth.fit.controller
 
+import com.birth.fit.dto.HelpListResponse
 import com.birth.fit.dto.PostRequest
 import com.birth.fit.service.HelpService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Pageable
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+
+
+
 
 @RestController
 @RequestMapping("/help")
 class HelpController(
     @Autowired val helpService: HelpService
 ) {
+
+    @GetMapping
+    fun getBoardList(@RequestHeader("Authorization") bearerToken: String?,
+                     pageable: Pageable): MutableList<HelpListResponse>? {
+        return helpService.getList(bearerToken, pageable)
+    }
 
     @PostMapping
     fun write(@RequestHeader("Authorization") bearerToken: String?,
