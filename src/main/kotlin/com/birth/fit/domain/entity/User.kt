a67b01a5859fa9c8f8cbed85cf8fc4be3594b09a
Hanif <marbling1293@naver.com>
2021-03-24 09:05:34 +0900

    [FIX] 접근 제어자? 수정

--- src/main/kotlin/com/birth/fit/domain/entity/User.kt
+++ src/main/kotlin/com/birth/fit/domain/entity/User.kt
@@ -8,16 +8,16 @@ class User(
 
     @Id
     @Column(name = "email")
-    var email: String,
+    internal val email: String,
 
     @Column(name = "user_id")
-    var userId: String,
+    internal var userId: String,
 
     @Column(name = "password")
-    var password: String,
+    internal var password: String,
 
     @OneToMany(mappedBy = "userEmail")
-    val help: List<Help>? = null
+    private val help: List<Help>? = null
 ) {
     fun update(userId: String, password: String) {
         this.userId = userId
