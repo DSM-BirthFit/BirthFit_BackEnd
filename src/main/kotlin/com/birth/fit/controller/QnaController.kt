7bd1c14609ee8c48dbd349c3785bb643f54a0834
Hanif <marbling1293@naver.com>
2021-03-27 00:44:41 +0900

    [ADD] QnaList response

--- src/main/kotlin/com/birth/fit/controller/QnaController.kt
+++ src/main/kotlin/com/birth/fit/controller/QnaController.kt
@@ -1,7 +1,11 @@
 package com.birth.fit.controller
 
+import com.birth.fit.dto.HelpContentResponse
+import com.birth.fit.dto.HelpListResponse
 import com.birth.fit.dto.PostRequest
+import com.birth.fit.dto.QnaListResponse
 import com.birth.fit.service.QnaService
+import org.springframework.data.domain.Pageable
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
@@ -10,6 +14,19 @@ import org.springframework.web.bind.annotation.*
 class QnaController(
     private val qnaService: QnaService
 ) {
+
+
+    @GetMapping
+    fun getList(@RequestHeader("Authorization") bearerToken: String?,
+                pageable: Pageable): MutableList<QnaListResponse>? {
+        return qnaService.getList(bearerToken, pageable)
+    }
+
+//    @GetMapping("/{helpId}")
+//    fun getContent(@RequestHeader("Authorization") bearerToken: String?,
+//                   @PathVariable @Validated helpId: Int): HelpContentResponse? {
+//        return qnaService.getContent(bearerToken, helpId)
+//    }
 
     @PostMapping
     fun write(@RequestHeader("Authorization") bearerToken: String?,
--- src/main/kotlin/com/birth/fit/service/QnaService.kt
+++ src/main/kotlin/com/birth/fit/service/QnaService.kt
@@ -2,13 +2,19 @@ package com.birth.fit.service
 
 import com.birth.fit.domain.entity.Qna
 import com.birth.fit.domain.entity.User
+import com.birth.fit.domain.repository.QnaAnswerRepository
+import com.birth.fit.domain.repository.QnaLikeRepository
 import com.birth.fit.domain.repository.QnaRepository
 import com.birth.fit.domain.repository.UserRepository
+import com.birth.fit.dto.HelpListResponse
 import com.birth.fit.dto.PostRequest
+import com.birth.fit.dto.QnaListResponse
 import com.birth.fit.exception.error.ExpiredTokenException
 import com.birth.fit.exception.error.UserNotFoundException
 import com.birth.fit.util.JwtTokenProvider
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
@@ -16,8 +22,35 @@ import java.time.LocalDateTime
 class QnaService(
     @Autowired private val qnaRepository: QnaRepository,
     @Autowired private val userRepository: UserRepository,
+    @Autowired private val qnaLikeRepository: QnaLikeRepository,
+    @Autowired private val qnaAnswerRepository: QnaAnswerRepository,
     @Autowired private val jwtTokenProvider: JwtTokenProvider
 ) {
+
+    fun getList(bearerToken: String?, pageable: Pageable): MutableList<QnaListResponse> {
+        val token: String? = jwtTokenProvider.resolveToken(bearerToken)
+        if(!jwtTokenProvider.validateToken(token!!)) throw ExpiredTokenException("The token has expired.")
+
+        val user: User? = userRepository.findByEmail(jwtTokenProvider.getUsername(token))
+        user?: throw UserNotFoundException("User not found.")
+
+        val qnaList: Page<Qna> = qnaRepository.findAll(pageable)
+        val list: MutableList<QnaListResponse> = ArrayList()
+
+        qnaList.let {
+            for(qna in it) {
+                list.add(
+                    QnaListResponse(
+                        qnaId = qna.id!!,
+                        title = qna.title,
+                        answer = qnaAnswerRepository.countByQnaId(qna.id!!),
+                        like = qnaLikeRepository.countByQnaId(qna.id!!)
+                    )
+                )
+            }
+        }
+        return list
+    }
 
     fun write(bearerToken: String?, postRequest: PostRequest) {
         val token: String? = jwtTokenProvider.resolveToken(bearerToken)
