69ceb5d27024389118ba1176dc6fb2fd76bc9ff3
Hanif <marbling1293@naver.com>
2021-03-26 11:30:01 +0900

    [ADD] qna entity

---
+++ src/main/kotlin/com/birth/fit/domain/entity/Qna.kt
@@ -0,0 +1,28 @@
+package com.birth.fit.domain.entity
+
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "qna")
+class Qna(
+    @Id
+    @Column(name = "qna_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    internal val id: Int? = null,
+
+    @Column(name = "user_email")
+    internal val userEmail: String,
+
+    @Column(name = "title")
+    internal var title: String,
+
+    @Column(name = "content")
+    internal var content: String,
+
+    @Column(name = "created_at")
+    internal val createdAt: LocalDateTime,
+
+    @Column(name = "view")
+    internal var view: Int = 0
+)
