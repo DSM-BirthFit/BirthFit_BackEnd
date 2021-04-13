d9f8f2c76454607e5acf09a2b717d6a3b7921a76
Hanif <marbling1293@naver.com>
2021-04-13 11:07:25 +0900

    [ADD] userImage response

--- src/main/kotlin/com/birth/fit/service/QnaService.kt
+++ src/main/kotlin/com/birth/fit/service/QnaService.kt
@@ -1,18 +1,24 @@
 package com.birth.fit.service
 
-import com.birth.fit.domain.entity.*
+import com.birth.fit.domain.entity.Qna
+import com.birth.fit.domain.entity.QnaAnswer
+import com.birth.fit.domain.entity.QnaLike
+import com.birth.fit.domain.entity.User
 import com.birth.fit.domain.repository.QnaAnswerRepository
 import com.birth.fit.domain.repository.QnaLikeRepository
 import com.birth.fit.domain.repository.QnaRepository
 import com.birth.fit.domain.repository.UserRepository
 import com.birth.fit.dto.*
-import com.birth.fit.exception.error.*
+import com.birth.fit.exception.error.ContentNotFoundException
+import com.birth.fit.exception.error.ExpiredTokenException
+import com.birth.fit.exception.error.PostNotFoundException
+import com.birth.fit.exception.error.UserNotFoundException
 import com.birth.fit.util.JwtTokenProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
-import java.time.LocalDateTime
+import java.time.LocalDate
 
 @Service
 class QnaService(
@@ -66,18 +72,17 @@ class QnaService(
 
         val list: MutableList<QnaAnswerResponse> = ArrayList()
         val answer: MutableList<QnaAnswer>? = qnaAnswerRepository.findAllByQnaId(qnaId)
-        answer?.run {
-            this.forEach {
-                val writer: User = userRepository.findByEmail(it.userEmail)!!
-                list.add(
-                    QnaAnswerResponse(
-                        qnaId = it.answerId!!,
-                        userId = writer.userId,
-                        content = it.content,
-                        isMine = writer.email == user.email
-                    )
+        answer?.forEach {
+            val writer: User = userRepository.findByEmail(it.userEmail)!!
+            list.add(
+                QnaAnswerResponse(
+                    qnaId = it.answerId!!,
+                    userId = writer.userId,
+                    userImage = writer.image,
+                    content = it.content,
+                    isMine = writer.email == user.email
                 )
-            }
+            )
         }
 
         qnaRepository.save(qna.view())
@@ -85,7 +90,8 @@ class QnaService(
             title = qna.title,
             content = qna.content,
             userId = author.userId,
-            createdAt = qna.createdAt.toString().substring(0, 10),
+            userImage = author.image,
+            createdAt = qna.createdAt.toString(),
             view = qna.view,
             likeCount = qna.likeCount,
             isMine = user.email == author.email,
@@ -106,7 +112,7 @@ class QnaService(
                 userEmail = user.email,
                 title = postRequest.title,
                 content = postRequest.content,
-                createdAt = LocalDateTime.now()
+                createdAt = LocalDate.now()
             )
         )
     }
