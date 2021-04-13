f5ffc0e24397f4776cd3ba4d26280f207621f24f
Hanif <marbling1293@naver.com>
2021-04-13 10:44:31 +0900

    [ADD] userImage request

--- src/main/kotlin/com/birth/fit/dto/ChangeProfileRequest.kt
+++ src/main/kotlin/com/birth/fit/dto/ChangeProfileRequest.kt
@@ -1,6 +1,9 @@
 package com.birth.fit.dto
 
+import org.springframework.web.multipart.MultipartFile
+
 class ChangeProfileRequest(
     val userId: String,
-    val password: String?
+    val password: String?,
+    val image: MultipartFile?
 )
