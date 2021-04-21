656500aea39d27cd7866415b3495172ac32bfa3b
Hanif <marbling1293@naver.com>
2021-04-21 19:14:37 +0900

    [ADD] Interceptor config

--- src/main/kotlin/com/birth/fit/common/config/CorsConfig.kt
+++ src/main/kotlin/com/birth/fit/common/config/CorsConfig.kt
@@ -1,17 +1,33 @@
 package com.birth.fit.common.config
 
+import com.birth.fit.common.util.JwtInterceptor
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.*
 
 @Configuration
 @EnableWebMvc
-class CorsConfig : WebMvcConfigurer {
+class CorsConfig(
+    @Autowired private val jwtInterceptor: JwtInterceptor
+) : WebMvcConfigurer {
+
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
             .allowedHeaders("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(jwtInterceptor)
+            .addPathPatterns("/*")
+            .excludePathPatterns("/user/join")
+            .excludePathPatterns("/user/login")
+            .excludePathPatterns("/user/password")
+            .excludePathPatterns("/email/**")
+            .excludePathPatterns("/swagger-ui.html/**",
+                "/swagger**", "/favicon**", "/webjars**",
+                "/webjars/**","swagger-ui-**", "/v2/**",
+                "/swagger-resources/**", "swagger-resources**")
     }
 }
