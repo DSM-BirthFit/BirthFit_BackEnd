a822fec093d7c3317e79d35ec235e68090053049
Hanif <marbling1293@naver.com>
2021-05-11 09:30:24 +0900

    [ADD] s3 upload byte input stream

--- src/main/kotlin/com/birth/fit/common/s3/S3Service.kt
+++ src/main/kotlin/com/birth/fit/common/s3/S3Service.kt
@@ -3,11 +3,14 @@ package com.birth.fit.common.s3
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.CannedAccessControlList
 import com.amazonaws.services.s3.model.PutObjectRequest
+import com.amazonaws.util.IOUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayInputStream
 import java.io.IOException
+
 
 @Component
 class S3Service(
@@ -18,8 +21,11 @@ class S3Service(
 
     @Throws(IOException::class)
     fun upload(file: MultipartFile, fileName: String?) {
+        val bytes: ByteArray = IOUtils.toByteArray(file.inputStream)
+        val byteArrayInputStream = ByteArrayInputStream(bytes)
+        
         s3Client.putObject(
-            PutObjectRequest(bucket, fileName, file.inputStream, null)
+            PutObjectRequest(bucket, fileName, byteArrayInputStream, null)
                 .withCannedAcl(CannedAccessControlList.PublicRead)
         )
     }
