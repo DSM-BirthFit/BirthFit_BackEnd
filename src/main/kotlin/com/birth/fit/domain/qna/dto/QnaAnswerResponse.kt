d272c0cd5764f673b9b723adc01c99f4af129aa9
Hanif <marbling1293@naver.com>
2021-05-25 13:48:47 +0900

    [ADD] 답변 id 반환

--- src/main/kotlin/com/birth/fit/domain/qna/dto/QnaAnswerResponse.kt
+++ src/main/kotlin/com/birth/fit/domain/qna/dto/QnaAnswerResponse.kt
@@ -1,6 +1,7 @@
 package com.birth.fit.domain.qna.dto
 
 class QnaAnswerResponse(
+    val answerId: Int,
     val userId: String,
     val userImage: String?,
     val answer: String,
--- src/main/kotlin/com/birth/fit/domain/qna/service/QnaService.kt
+++ src/main/kotlin/com/birth/fit/domain/qna/service/QnaService.kt
@@ -83,6 +83,7 @@ class QnaService(
             answer = qnaAnswerRepository.findAllByQnaId(qnaId)?.map {
                 val writer: User = userRepository.findByEmail(it.userEmail)!!
                 QnaAnswerResponse(
+                    answerId = it.answerId!!,
                     userId = writer.userId,
                     userImage = writer.image,
                     answer = it.answer,
